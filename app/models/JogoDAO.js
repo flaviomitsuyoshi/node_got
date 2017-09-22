17f71b2c5981d8f49ca9018908aa4bd03512ca7e
Flaviota <flaviomitsuyoshi@gmail.com>
2017-09-22 00:07:35 -0300

    revogar ordem

--- app/models/JogoDAO.js
+++ app/models/JogoDAO.js
@@ -1,3 +1,5 @@
+var ObjectID = require('mongodb').ObjectId;
+
 function JogoDAO(connection){
 	this._connection = connection();
 }
@@ -87,6 +89,21 @@ JogoDAO.prototype.getAcoes = function(usuario, res) {
 
 				mongoclient.close();
 			});
+			
+		});
+	});
+}
+
+JogoDAO.prototype.revogarAcao = function(_id, res) {
+	this._connection.open( function(err, mongoclient){
+		mongoclient.collection("acao", function(err, collection){
+			collection.remove(
+				{_id: ObjectID(_id)},
+				function(err, result) {
+					res.redirect("jogo?msg=D");
+					mongoclient.close();
+				}
+			);
 			
 		});
 	});
